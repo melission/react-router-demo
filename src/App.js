9fdd40e93892b62620a11de812206c4aa343bf47
Polina <polina@blsv.space>
2023-04-24 11:49:24 +0200

    created 3 conts arr func: Home, Category, Products.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+);
+
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='navbar navbar-light'>
+        <ul className='nav navbar-nav'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/category">Category</Link>
+          </li>
+          <li>
+            <Link to="products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* use *exact* otherwise all pathes starting with '/'' match */}
+      <Route exact path="/"><Home></Home></Route>
+      <Route path="/categpry"><Category></Category></Route>
+      <Route path="/products"><Products></Products></Route>
     </div>
   );
 }
