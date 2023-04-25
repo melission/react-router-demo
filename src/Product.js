481d843fcc094c886fec62af1429dc7cc1b25f70
Polina <polina@blsv.space>
2023-04-25 16:02:40 +0200

    created, has const productData with an array.

---
+++ src/Product.js
@@ -0,0 +1,20 @@
+const productData = [
+    {
+        id: 1,
+        name: "name one",
+        description: "desc one",
+        status: "Available",
+    },
+    {
+        id: 2,
+        name: "name two",
+        description: "desc two",
+        status: "Not available",
+    },
+    {
+        id: 3,
+        name: "name three",
+        description: "desc three",
+        status: "Not available",
+    },
+]
