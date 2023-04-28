8f67ed810895702eba3fbe22796bd65b24b83f9c
Polina <polina@blsv.space>
2023-04-28 12:35:23 +0200

    const Product was deleted; imported Products; Outlet was imported and added into App to render children elements; Routes were commented out and the whole route system was changed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { Link, Route, Routes } from 'react-router-dom';
-import Category from './Category'
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
+import Category from './Category';
+import Products from "./Products";
 
 const Home = () => (
   <div>
@@ -8,29 +9,36 @@ const Home = () => (
   </div>
 );
 
-const Products = () => (
-  <div>
-    <h2>Products</h2>
-  </div>
-);
-
 function App() {
   return (
     <div className="App">
-      <nav className='navbar navbar-light'>
-        <ul className='nav navbar-nav'>
-          <li>
-            <Link to="/">Home</Link>
+      <head>
+      <link rel="stylesheet" 
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" 
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+      crossorigin="anonymous"></link>
+      </head>
+      {/* <nav className='navbar navbar-light'> */}
+        <ul className='nav justify-content-center bg-light'>
+          <li className='nav-item'>
+            <a className='nav-link mt-0' >
+              <Link to={`/`}>Home</Link>
+            </a>
           </li>
-          <li>
-            <Link to="/category">Category</Link>
+            <li className='nav-item'>          
+              <a className='nav-link'>
+                <Link to={`category/`}>Category</Link>
+              </a>            
           </li>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
+            <li className='nav-item'>
+              <a className='nav-link'>
+                <Link to={`products/`}>Products</Link>
+              </a>
+            </li>
         </ul>
-      </nav>
-
+      {/* </nav> */}
+      <h2>the end of root page</h2>
+      <Outlet />
       {/* use *exact* otherwise all pathes starting with '/'' match
       <Routes>
         <Route exact path="/"><Home /></Route>
@@ -38,11 +46,11 @@ function App() {
         <Route path="/products"><Products /></Route>
       </Routes> */}
 
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home/>}></Route>
-        <Route path="category/" element={<Category />}></Route>
-        <Route path="products/" element={<Products />}></Route>
-      </Routes>
+        <Route path="category/*" element={<Category />}></Route>
+        <Route path="products/*" element={<Products />}></Route>
+      </Routes> */}
     </div>
   );
 }
