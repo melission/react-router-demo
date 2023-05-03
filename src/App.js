3fec9f3d06fb16c49b7ba67bc4023d2e7ea99789
Polina <polina@blsv.space>
2023-05-03 12:41:45 +0200

    navbar was replaced with bootstrap Navbar component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { Link, Outlet, Route, Routes } from 'react-router-dom';
 import Category from './Category';
 import Products from "./Products";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 const Home = () => (
   <div>
@@ -18,40 +22,20 @@ function App() {
       integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
       crossorigin="anonymous"></link>
       </head>
-      {/* <nav className='navbar navbar-light'> */}
-        <ul className='nav justify-content-center bg-light'>
-          <li className='nav-item'>
-            <a className='nav-link mt-0' >
-              <Link to={`/`}>Home</Link>
-            </a>
-          </li>
-            <li className='nav-item'>          
-              <a className='nav-link'>
-                <Link to={`category/`}>Category</Link>
-              </a>            
-          </li>
-            <li className='nav-item'>
-              <a className='nav-link'>
-                <Link to={`products/`}>Products</Link>
-              </a>
-            </li>
-        </ul>
-      {/* </nav> */}
-      <h2>the end of root page</h2>
-      <Outlet />
-      {/* use *exact* otherwise all pathes starting with '/'' match
-      <Routes>
-        <Route exact path="/"><Home /></Route>
-        <Route path="/categpry"><Category /></Route>
-        <Route path="/products"><Products /></Route>
-      </Routes> */}
 
-      {/* <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="category/*" element={<Category />}></Route>
-        <Route path="products/*" element={<Products />}></Route>
-      </Routes> */}
-    </div>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href={`/`}>Basic Page</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href={`/category/`}>Category</Nav.Link>
+            <Nav.Link href={`/products/`}>Products</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Outlet />
+
+      </div>
   );
 }
 
