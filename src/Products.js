cd62e37c068fde87b09f2ddf65bcdca991309f67
Polina <polina@blsv.space>
2023-05-15 11:16:20 +0200

    added a ShortDescription arrow function.

--- src/Products.js
+++ src/Products.js
@@ -4,6 +4,18 @@ import productData from "./productData";
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 
+const ShortDescription = ({ product }) => {
+    let text = product.description
+    console.log(typeof text.length)
+    if (text.length > 100) {        
+        console.log("yes")
+        return text.slice(0, 100)
+
+    } else {
+        console.log("no")
+        return text
+    }
+}
 
 const Products = ({ match }) => {
     const linkList = productData.map((product) => {
@@ -13,7 +25,7 @@ const Products = ({ match }) => {
                   <Card.Title>{product.name}</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">{product.status}</Card.Subtitle>
                   <Card.Text>
-                    Here is a quick product description: {product.description}
+                    Here is a quick product description: <ShortDescription product={product} />
                   </Card.Text>
                   <Card.Link href={`/products/${product.id}`}>See detailes</Card.Link>
                 </Card.Body>
