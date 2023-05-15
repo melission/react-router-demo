8e8a830b3cef196e40c5d9ea009a4078ec92b3c5
Polina <polina@blsv.space>
2023-05-15 11:14:54 +0200

    added Alert, Button, "Product.css", and BreabcrumbProduct imports. created Status arr func that uses [bootstrap] Alert to reflect status of a product.

--- src/Product.js
+++ src/Product.js
@@ -5,11 +5,34 @@ import image1 from "./shoes.png";
 import image2 from "./sneakers.png";
 import image3 from "./football-boots.png";
 import Carousel from 'react-bootstrap/Carousel';
+import Alert from 'react-bootstrap/Alert';
+import Button from "react-bootstrap/Button";
+import "./Product.css"
+import BreadcrumbProduct from "./breadcrumbs";
+
+const Status = ({product}) => {
+    if (product.status === "Available") {
+        return (
+            <Alert variant="success">
+                <Alert.Heading>Available</Alert.Heading>
+            </Alert>
+        )
+    } else {
+        return (
+            <Alert variant="danger">
+                <Alert.Heading>Out of stock</Alert.Heading>
+            </Alert>
+        )
+    }
+}
+
+
+
 
 const Product = ( ) => {
     const {productID} = useParams();
     const product = productData.find(p => p.id === Number(productID));
-    console.log(product)
+    // console.log(product.id)
 
     let productInfo;
 
@@ -21,45 +44,58 @@ const Product = ( ) => {
             //     <p>{ product.description }</p>
             //     <hr></hr>
             //     <h4>{product.status}</h4>
-            // </div>
-            <div>
-                <h2>{product.name}</h2>
-                <h3></h3>
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-30"
-                        src={image1}
-                        alt="First slide"
-                        />
-                        {/* <Carousel.Caption>
-                        <h3>{product.name} slide one</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption> */}
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-30"
-                        src={image2}
-                        alt="Second slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-30"
-                        src={image3}
-                        alt="Third slide"
-                        />
-                    </Carousel.Item>
-                </Carousel>
+            // </div
+
+            <div class="product-body">
+                <div className="product-main">
+                    <h2>{product.name}</h2>
+                    <Status product={product} />
+                    <p class="product-desc">
+                        {product.description}
+                    </p>
+                    <div className="carusel-wrapper">
+                        <Carousel className="border" variant="dark">
+                            <Carousel.Item>
+                                <img
+                                className="d-block w-90"
+                                src={image1}
+                                alt="First slide"
+                                />
+                                {/* <Carousel.Caption>
+                                <h3>{product.name} slide one</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption> */}
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                className="d-block w-90"
+                                src={image2}
+                                alt="Second slide"
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                className="d-block w-90"
+                                src={image3}
+                                alt="Third slide"
+                                />
+                            </Carousel.Item>
+                        </Carousel>
+                    </div>
+                </div>
             </div>
         );
     } else {
         productInfo = <h2>Sorry. The product doesn't excist.</h2>
     }
 
+    let currentID = product.id
+    console.log("before return", currentID)
     return (
         <div>
+            <div class="product-breadcrumbs">
+                <BreadcrumbProduct current={currentID}></BreadcrumbProduct>
+            </div>
             <div>{productInfo}</div>
         </div>
     )
