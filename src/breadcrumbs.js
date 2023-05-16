4c6b116875b1802bd3e55f652894a20a65780770
Polina <polina@blsv.space>
2023-05-16 16:04:21 +0200

    added BreadcrumbCategory func.

--- src/breadcrumbs.js
+++ src/breadcrumbs.js
@@ -4,7 +4,7 @@ const BreadcrumbProduct = ({product}) => {
 
     // let currentId = product.id
 
-    console.log(product)
+    // console.log(product)
 
     return (
         <Breadcrumb>
@@ -17,4 +17,15 @@ const BreadcrumbProduct = ({product}) => {
     );
 }
 
-export default BreadcrumbProduct;
+const BreadcrumbCategory = ({}) => {
+    return (
+        <Breadcrumb>
+            <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+            <Breadcrumb.Item href={`/category`}>
+                Category
+            </Breadcrumb.Item>
+        </Breadcrumb>
+    )
+}
+
+export {BreadcrumbProduct, BreadcrumbCategory};
