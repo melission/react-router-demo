029dc59777dd319c69cd01e5e75dd9934e1a0873
Polina <polina@blsv.space>
2023-05-16 13:42:09 +0200

    deleted unused code.

--- src/Category.js
+++ src/Category.js
@@ -13,50 +13,11 @@ const Item = () => {
     );
 };
 
-// const Category = () => {
-//     const {url, path} = useRouteMatch();
-
-//     return (
-//         <div>
-//             <ul>
-//                 <li>
-//                     <Link to={`${url}/shoes`}>Shoes</Link>
-//                 </li>
-//                 <li>
-//                     <Link to={`${url}/boots`}>Boots</Link>
-//                 </li>
-//                 <li>
-//                     <Link to={`${url}/footwear`}>Footwear</Link>
-//                 </li>
-//             </ul>
-//             <Route path={`${path}/:name`}>
-//                 <Item />
-//             </Route>
-//         </div>
-//     );
-// };
-
 const Category = () => {
 
     return (
         <div> 
-            <ul>
-                <li>
-                    <Link to="shoes/*">Shoes</Link>
-                </li>
-                <li>
-                    <Link to="boots/*">Boots</Link>
-                </li>
-                <li>
-                    <Link to="footwear/*">Foorwear</Link>
-                </li>
-            </ul>
 
-            {/* <Routes>
-                <Route path="shoes/*" element={<Item />} />
-                <Route path="boots/*" element={<Item />} />
-                <Route path="footwear/*" element={<Item />} />
-            </Routes> */}
         </div>
     )
     }
