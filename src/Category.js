af4424e41a42f27d9cb319a3257e3aaef3b0811f
Polina <polina@blsv.space>
2023-05-18 11:25:31 +0200

    created CardFactory arrow function; Category const was filled with code.

--- src/Category.js
+++ src/Category.js
@@ -1,14 +1,41 @@
 import React from "react";
-import { Link, Route, Routes, useParams } from "react-router-dom";
-import {BreadcrumbCategory} from "./breadcrumbs";
+import {Outlet} from "react-router-dom";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Card from "react-bootstrap/Card";
+// import { CategoryList } from "./CardFactory";
 
+
+const categoryList = ["books"]
+
+const CardFactory = (categoryItem) => {
+    console.log(categoryItem)
+    return(
+        <Card className="card-body" border="light" style={{ height: "400px" }}>
+            <Card.Body>
+                <Card.Title>{categoryItem}</Card.Title>
+            </Card.Body>
+            <Card.Footer>
+                <Card.Link href={`/category/${categoryItem}/`}>See detailes</Card.Link>
+            </Card.Footer>
+        </Card>
+    )
+}
 
 const Category = () => {
-
+    // const categories = categoryList.map(category => CategoryList(category, ))
+    const categories = CardFactory(categoryList)
     return (
         <div>
             {/* <BreadcrumbCategory></BreadcrumbCategory> */}
-            
+            <Container>
+                <Row>
+                    {categories}
+                </Row>
+            </Container>
+            <div>
+                <Outlet></Outlet>
+            </div>
         </div>
     )
     }
