daf5e9644c64ab2c3488b3d98b88ab85fab368bb
Polina <polina@blsv.space>
2023-05-30 10:48:04 +0200

    added an image import and its usage

--- src/Category.js
+++ src/Category.js
@@ -3,15 +3,17 @@ import {Outlet} from "react-router-dom";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Card from "react-bootstrap/Card";
+import bookImage from "./images/books/reading-book.png";
 // import { CategoryList } from "./CardFactory";
 
 
 const categoryList = ["books"]
 
 const CardFactory = (categoryItem) => {
-    console.log(categoryItem)
+    // console.log(categoryItem)
     return(
         <Card className="card-body" border="light" style={{ height: "400px" }}>
+            <Card.Img variant="top" src={bookImage} />
             <Card.Body>
                 <Card.Title>{categoryItem}</Card.Title>
             </Card.Body>
