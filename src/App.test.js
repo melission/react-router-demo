0989217903ee88056ed472e8a0a0ef44b98be8ca
Polina <polina@blsv.space>
2023-06-01 12:11:58 +0200

    created; contains several route tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders navbar with correct links', () => {
+    render(<App />);
+
+    const homeLink = screen.getByText('Basic Page');
+    const categoryLink = screen.getByText('Category');
+    const productsLink = screen.getByText('Products');
+    const factoryLink = screen.getByText('Factory');
+    const loginLink = screen.getByText('Log in');
+
+    expect(homeLink.getAttribute('href')).toBe('/');
+    expect(categoryLink.getAttribute('href')).toBe('/category/');
+    expect(productsLink.getAttribute('href')).toBe('/products/');
+    expect(factoryLink.getAttribute('href')).toBe('/factory/');
+    expect(loginLink.getAttribute('href')).toBe('/login/');
+  });
+
+
 });
