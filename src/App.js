e6ba96c0a4c63d4059712ee9e12c89d55e5cef93
Polina <polina@blsv.space>
2023-08-16 13:28:26 +0200

    the structure was reworked: main component is ToggleColorMode; the functional for having "light" and "dark" themes was added.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import {Outlet} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import "./index.css"
+import "./index.css";
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 function App() {
+
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+  // console.log(colorMode)
+
   return (
-    <div className="App">
+    <Box className="App"
+    sx={{
+      // display: 'flex',
+      // width: '100%',
+      // alignItems: 'center',
+      // justifyContent: 'center',
+      bgcolor: 'background.default',
+      color: 'text.primary',
+      // borderRadius: 1,
+      // p: 3,
+    }}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -27,12 +50,49 @@ function App() {
             <Nav.Link href={`/factory/`}>Factory</Nav.Link>
             <Nav.Link href={`/login/`} >Log in</Nav.Link>
           </Nav>
-          <Button variant="outline-dark" onClick={toggleTheme} >Dark Theme</Button>
+          {theme.palette.mode} mode
+        <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+          {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
         </Container>
       </Navbar>
+      {/* <ToggleColorMode /> */}
       <Outlet />
-      </div>
+      </Box>
   );
 }
 
-export default App;
+export default function ToggleColorMode() {
+  const storedMode = localStorage.getItem('colorMode')
+
+  //changes each time when icon button is clicked
+  const [mode, setMode] = React.useState(storedMode || 'light');
+
+  // Save the color mode to local storage whenever it changes
+  React.useEffect(() => {
+    localStorage.setItem('colorMode', mode);
+  }, [mode]);
+
+  const toggleColorMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  const colorMode = {
+    toggleColorMode,
+  };
+
+  const theme = createTheme({
+    palette: {
+      mode,
+    },
+  });
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
