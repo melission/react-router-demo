f951516c3208ec8c245b6d6ffabcc45202c515db
Polina <polina@blsv.space>
2023-08-16 09:11:13 +0200

    "index.css" was imported

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Outlet} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import "./index.css"
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
             <Nav.Link href={`/factory/`}>Factory</Nav.Link>
             <Nav.Link href={`/login/`} >Log in</Nav.Link>
           </Nav>
+          <Button variant="outline-dark" onClick={toggleTheme} >Dark Theme</Button>
         </Container>
       </Navbar>
       <Outlet />
