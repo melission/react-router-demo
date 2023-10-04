5aa713e5d0db74db92b1ca959e5e2c0b7267d57e
Polina <polina@blsv.space>
2023-10-04 13:33:08 +0200

    added terinary operator for a button styles; imported theme; added "shadow" section class.

--- src/leisureIdeas/IdeaGenerator.js
+++ src/leisureIdeas/IdeaGenerator.js
@@ -3,8 +3,11 @@ import Button from 'react-bootstrap/esm/Button';
 import ideas from "./ideas";
 import "./ideas.css"
 
+const storedMode = localStorage.getItem('colorMode')
+
 const IdeaGenerator = () => {
   const [randomIdea, setRandomIdea] = useState('');
+  let buttonTheme = storedMode === 'dark' ? 'outline-light' : "outline-dark"
 
   const pickRandomIdea = () => {
     const randomIndex = Math.floor(Math.random() * ideas.length);
@@ -16,10 +19,10 @@ const IdeaGenerator = () => {
   return (
     <div className='idea-block text-center'>
       <h2 className='idea-block_title'>Random Idea Generator</h2>
-      <section className='idea-block_box'>
+      <section className='idea-block_box idea-block_shadow'>
         <div className='idea-box_text'>{randomIdea}</div>
       </section>
-      <Button className='idea-button' variant="outline-dark" onClick={pickRandomIdea}>Generate Idea</Button>
+      <Button className='idea-button' variant={buttonTheme} onClick={pickRandomIdea}>Generate Idea</Button>
     </div>
   );
 };
