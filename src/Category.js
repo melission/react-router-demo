6e87b74c80cb2278477035db7559375812f07eac
Polina <polina@blsv.space>
2023-10-09 14:06:10 +0200

    removed unused code; added a little bit of hierarchy for better readability.

--- src/Category.js
+++ src/Category.js
@@ -4,37 +4,41 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Card from "react-bootstrap/Card";
 import bookImage from "./images/books/reading-book.png";
-import BookOutlinedIcon from '@mui/icons-material/BookOutlined';
-// import { CategoryList } from "./CardFactory";
 
 const storedMode = localStorage.getItem('colorMode')
 
 const categoryList = ["books"]
 
 const CardFactory = (categoryItem) => {
-    // console.log(categoryItem)
-    let cardBG = storedMode === 'dark' ? "secondary" : "transparent"
     let cardText = storedMode === "dark" ? "text-light" : "text-secondary"
-
     return (
-        <Card className="card-body" variant={storedMode} border={storedMode} bg={cardBG} style={{ height: "400px" }}>
+        <Card
+            className="card-body"
+            variant={storedMode}
+            border={storedMode}
+            bg={'transparent'}
+            style={{ height: "400px" }}>
             <Card.Img variant="top" src={bookImage} />
             <Card.Body>
                 <Card.Title>{categoryItem}</Card.Title>
             </Card.Body>
             <Card.Footer>
-                <Card.Link className={cardText}><Link className="text-decoration-none" style={{ color: "inherit" }} to={`/category/${categoryItem}/`}>See detailes</Link></Card.Link>
+                <Card.Link className={cardText}>
+                    <Link
+                        className="text-decoration-none"
+                        style={{ color: "inherit" }}
+                        to={`/category/${categoryItem}/`}>See detailes
+                    </Link>
+                </Card.Link>
             </Card.Footer>
         </Card>
     )
 }
 
 const Category = () => {
-    // const categories = categoryList.map(category => CategoryList(category, ))
     const categories = CardFactory(categoryList)
     return (
         <div>
-            {/* <BreadcrumbCategory></BreadcrumbCategory> */}
             <Container>
                 <Row>
                     {categories}
