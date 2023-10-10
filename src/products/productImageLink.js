cd6f2fc9ad2dadd08a47877a033e657eb38502f0
Polina <polina@blsv.space>
2023-10-10 11:58:31 +0200

    created; contains 'productImage' with 9 ids.

---
+++ src/products/productImageLink.js
@@ -0,0 +1,63 @@
+import sneakers1 from './images/shoes/football-boots.png'
+import sneakers2 from './images/shoes/shoes.png'
+import sneakers3 from './images/shoes/sneakers.png'
+import star from './images/miscellaneous/camping.png'
+
+const productImage = [
+    {
+        id: 1,
+        images: [
+            sneakers1, sneakers2, sneakers3
+        ]
+    },
+    {
+        id: 2,
+        images: [
+            sneakers1, sneakers2, sneakers3
+        ]
+    },
+    {
+        id: 3,
+        images: [
+            sneakers1, sneakers2, sneakers3
+        ]
+    },
+    {
+        id: 4,
+        images: [
+            star
+        ]
+    },
+    {
+        id: 5,
+        images: [
+            sneakers1, sneakers2, sneakers3
+        ]
+    },
+    {
+        id: 6,
+        images: [
+            sneakers1, sneakers2, sneakers3
+        ]
+    },
+    {
+        id: 7,
+        images: [
+            sneakers1, sneakers2, sneakers3
+        ]
+    },
+    {
+        id: 8,
+        images: [
+            sneakers1, sneakers2, sneakers3
+        ]
+    },
+    {
+        id: 9,
+        images: [
+            sneakers1, sneakers2, sneakers3
+        ]
+    }
+]
+
+export default productImage
