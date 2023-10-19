1cf878a1555c3d3d5aeb0ee6bd9ee39e55baa693
Polina <polina@blsv.space>
2023-10-19 12:23:45 +0200

    react-bootstrap was replaced with MUI. No other changes

--- src/main-page/Resources.js
+++ src/main-page/Resources.js
@@ -1,23 +1,20 @@
 import React from "react";
-import Container from "react-bootstrap/esm/Container";
-import Row from "react-bootstrap/esm/Row";
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import StyledItem from "./StyledItem";
 
 const ResourceBlock = () => {
     const desc = 'The site was created using following resources: '
     const resourceList = ['https://www.flaticon.com/', 'https://chat.openai.com/']
     return (
-        <Container>
-            <Row>
-                {desc}
-            </Row>
-            <Row>
-                <ul>
-                    {resourceList.map((resource) => (
-                        <li>{resource}</li>
-                    ))}
-                </ul>
-            </Row>
-        </Container>
+        <Box>
+            {desc}
+            <Stack spacing={2}>
+                {resourceList.map((resource) => (
+                    <StyledItem>{resource}</StyledItem>
+                ))}
+            </Stack>
+        </Box>
     )
 }
 
