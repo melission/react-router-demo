2e925351bb75dae93fa32cf5246bf44db5a0e312
Polina <polina@blsv.space>
2023-10-19 12:26:37 +0200

    imported UselessFactWidget; changed grip height values.

--- src/main-page/Main.js
+++ src/main-page/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ResourceBlock from "./Resources";
+import UselessFactWidget from "./UselessFactWidget";
 
 const MainPage = () => {
 
@@ -8,13 +9,13 @@ const MainPage = () => {
     return (
         <>
             <div class="grid grid-cols-1 md:grid-cols-3 mt-4 grid-rows-4 gap-4 h-[80vh] w-full gap-y-2 p-3 bg-slate-100">
-                <div class="box row-span-2 h-[10%]">{firstDiv}</div>
-                <div class="box row-start-3 h-[10%]">2</div>
-                <div class="box row-start-4 h-[10%]">3</div>
+                <div class="box row-span-2 h-full">{firstDiv}</div>
+                <div class="box row-start-3 h-full">2</div>
+                <div class="box row-start-4 h-full flex items-center justify-center"><UselessFactWidget /></div>
                 <div class="box row-span-4 h-full bg-red-50">4</div>
-                <div class="box row-span-2 h-[10%]">5</div>
-                <div class="box h-[10%]">6</div>
-                <div class="box h-[10%]"><ResourceBlock /></div>
+                <div class="box row-span-2 h-full flex items-center justify-center"></div>
+                <div class="box h-full">6</div>
+                <div class="box h-full flex items-center justify-center" ><ResourceBlock /></div>
             </div>
             <footer class="bg-gray-800 absolute bottom-0 w-full text-white text-center m-0 p-4">
                 &copy; 2023 Polin Belousova.
