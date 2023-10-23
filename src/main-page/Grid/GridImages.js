831c7a509286aec10d16a5e59a5db323238b486f
Polina <polina@blsv.space>
2023-10-23 14:17:24 +0200

    created; contains GridImages.

---
+++ src/main-page/Grid/GridImages.js
@@ -0,0 +1,13 @@
+import book from '../images/grid/book.png'
+import brain from '../images/grid/brainstorm.png'
+import rocket from '../images/grid/rocket.png'
+import tablet from '../images/grid/drawing-tablet.png'
+
+const GridImages = [
+    { name: 'Element 1', image: book },
+    { name: 'Element 2', image: brain },
+    { name: 'Element 3', image: rocket },
+    { name: 'Element 4', image: tablet }
+]
+
+export default GridImages
