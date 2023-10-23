14a216d40b68adf24fe6accc737b07a125a849b1
Polina <polina@blsv.space>
2023-10-23 14:16:10 +0200

    added GridWithHoverBorders; added snap to footer; removed a few bg colours.

--- src/main-page/Main.js
+++ src/main-page/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import GridWithHoverBorders from "./Grid/Grid";
 import ResourceBlock from "./Resources";
 import UselessFactWidget from "./UselessFactWidget";
 
@@ -8,17 +9,19 @@ const MainPage = () => {
 
     return (
         <>
-            <div class="grid grid-cols-1 md:grid-cols-3 mt-4 grid-rows-4 gap-4 h-[80vh] w-full gap-y-2 p-3 bg-slate-100">
+            <div class="grid grid-cols-1 md:grid-cols-3 mt-4 grid-rows-4 gap-4 h-[80vh] w-full gap-y-2 p-3">
                 <div class="box row-span-2 h-full">{firstDiv}</div>
                 <div class="box row-start-3 h-full">2</div>
                 <div class="box row-start-4 h-full flex items-center justify-center"><UselessFactWidget /></div>
                 <div class="box row-span-4 h-full bg-red-50">4</div>
-                <div class="box row-span-2 h-full flex items-center justify-center"></div>
+                <div class="box row-span-2 h-full flex items-center justify-center"><GridWithHoverBorders /></div>
                 <div class="box h-full">6</div>
                 <div class="box h-full flex items-center justify-center" ><ResourceBlock /></div>
             </div>
-            <footer class="bg-gray-800 absolute bottom-0 w-full text-white text-center m-0 p-4">
-                &copy; 2023 Polin Belousova.
+            <footer class="text-xl font-semibold italic absolute bottom-0 w-full text-center m-0 p-4">
+                &copy; 2023 <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+                    <span class="relative text-white mx-2">Polin Belousova.</span>
+                </span>
             </footer>
         </>
     )
