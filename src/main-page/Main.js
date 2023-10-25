2a4ba7cbf2e154624095576ebcb095793330aede
Polina <polina@blsv.space>
2023-10-25 16:06:26 +0200

    added GitHubLogog

--- src/main-page/Main.js
+++ src/main-page/Main.js
@@ -4,20 +4,21 @@ import ResourceBlock from "./Resources";
 import UselessFactWidget from "./UselessFactWidget";
 import TimeWidget from "./TimeWidget";
 import IdeaGenerator from "../leisureIdeas/IdeaGenerator";
+import GitHubLogo from "./InfoField";
 
 const MainPage = () => {
 
     const firstDiv = `“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius vitae ipsum et commodo. In scelerisque est magna, ut fringilla purus congue eu. Mauris id metus ac metus porta aliquet”`
 
     return (
-        <div class="overflow-y-auto">
+        <div class="overflow-y-auto md:overflow-hidden">
             <div class="grid grid-cols-1 md:grid-cols-3 mt-4 grid-rows-4 gap-4 md:h-[80vh] w-full gap-y-2 p-3">
                 <div class="box row-span-2 h-full">{firstDiv}</div>
                 <div class="box row-start-3 h-full"><TimeWidget /></div>
                 <div class="box row-start-4 h-full flex items-center justify-center"><UselessFactWidget /></div>
                 <div class="box row-span-4 h-full"><IdeaGenerator /></div>
                 <div class="box row-span-2 h-full flex items-center justify-center"><GridWithHoverBorders /></div>
-                <div class="box h-full">6</div>
+                <div class="box h-full"><GitHubLogo /></div>
                 <div class="box h-full flex items-center justify-center" ><ResourceBlock /></div>
             </div>
             <footer class="text-xl font-semibold italic absolute md:bottom-0 w-full text-center m-0 p-4">
